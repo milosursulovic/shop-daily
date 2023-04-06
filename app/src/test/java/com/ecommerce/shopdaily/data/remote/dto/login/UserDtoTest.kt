985391774b500fde416d4a6b46c125bb87ebaea9
Misa <milosursulovic2696@gmail.com>
2023-04-06 18:04:14 +0200

    Add unit test for user dto

---
+++ app/src/test/java/com/ecommerce/shopdaily/data/remote/dto/login/UserDtoTest.kt
@@ -0,0 +1,41 @@
+package com.ecommerce.shopdaily.data.remote.dto.login
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class UserDtoTest {
+    @Test
+    fun `user properties should match the provided values`() {
+        // Arrange
+        val userDto = UserDto(
+            email = "user@example.com",
+            firstName = "John",
+            gender = "male",
+            id = 1,
+            image = "https://example.com/user.jpg",
+            lastName = "Doe",
+            token = "abc123",
+            username = "johndoe"
+        )
+
+        // Act
+        val actualEmail = userDto.email
+        val actualFirstName = userDto.firstName
+        val actualGender = userDto.gender
+        val actualId = userDto.id
+        val actualImage = userDto.image
+        val actualLastName = userDto.lastName
+        val actualToken = userDto.token
+        val actualUsername = userDto.username
+
+        // Assert
+        assertThat(actualEmail).isEqualTo("user@example.com")
+        assertThat(actualFirstName).isEqualTo("John")
+        assertThat(actualGender).isEqualTo("male")
+        assertThat(actualId).isEqualTo(1)
+        assertThat(actualImage).isEqualTo("https://example.com/user.jpg")
+        assertThat(actualLastName).isEqualTo("Doe")
+        assertThat(actualToken).isEqualTo("abc123")
+        assertThat(actualUsername).isEqualTo("johndoe")
+    }
+}

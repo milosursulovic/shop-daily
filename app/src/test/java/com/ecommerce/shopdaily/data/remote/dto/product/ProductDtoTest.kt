f8ff55c64c5c5d3784ab4cda50497cb9b2b9574e
Misa <milosursulovic2696@gmail.com>
2023-04-06 18:08:02 +0200

    Add unit test for product dto

---
+++ app/src/test/java/com/ecommerce/shopdaily/data/remote/dto/product/ProductDtoTest.kt
@@ -0,0 +1,50 @@
+package com.ecommerce.shopdaily.data.remote.dto.product
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class ProductDtoTest {
+    @Test
+    fun `product properties should match the provided values`() {
+        // Arrange
+        val productDto = ProductDto(
+            brand = "Apple",
+            category = "Electronics",
+            description = "The latest iPhone",
+            discountPercentage = 0.2,
+            id = 1,
+            images = listOf("https://example.com/product.jpg"),
+            price = 1000,
+            rating = 4.5,
+            stock = 10,
+            thumbnail = "https://example.com/product-thumbnail.jpg",
+            title = "iPhone 12"
+        )
+
+        // Act
+        val actualBrand = productDto.brand
+        val actualCategory = productDto.category
+        val actualDescription = productDto.description
+        val actualDiscountPercentage = productDto.discountPercentage
+        val actualId = productDto.id
+        val actualImages = productDto.images
+        val actualPrice = productDto.price
+        val actualRating = productDto.rating
+        val actualStock = productDto.stock
+        val actualThumbnail = productDto.thumbnail
+        val actualTitle = productDto.title
+
+        // Assert
+        assertThat(actualBrand).isEqualTo("Apple")
+        assertThat(actualCategory).isEqualTo("Electronics")
+        assertThat(actualDescription).isEqualTo("The latest iPhone")
+        assertThat(actualDiscountPercentage).isEqualTo(0.2)
+        assertThat(actualId).isEqualTo(1)
+        assertThat(actualImages).containsExactly("https://example.com/product.jpg")
+        assertThat(actualPrice).isEqualTo(1000)
+        assertThat(actualRating).isEqualTo(4.5)
+        assertThat(actualStock).isEqualTo(10)
+        assertThat(actualThumbnail).isEqualTo("https://example.com/product-thumbnail.jpg")
+        assertThat(actualTitle).isEqualTo("iPhone 12")
+    }
+}

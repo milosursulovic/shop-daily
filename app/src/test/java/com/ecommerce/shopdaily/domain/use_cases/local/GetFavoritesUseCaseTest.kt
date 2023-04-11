7b126f7f8901c10c81df9665f92bd0390c5ad2ac
Misa <milosursulovic2696@gmail.com>
2023-04-11 13:57:47 +0200

    Add unit test for get favorites use case #37

---
+++ app/src/test/java/com/ecommerce/shopdaily/domain/use_cases/local/GetFavoritesUseCaseTest.kt
@@ -0,0 +1,77 @@
+package com.ecommerce.shopdaily.domain.use_cases.local
+
+import com.ecommerce.shopdaily.common.Resource
+import com.ecommerce.shopdaily.data.db.entities.ProductEntity
+import com.ecommerce.shopdaily.data.mappers.product.toProduct
+import com.ecommerce.shopdaily.domain.model.product.Product
+import com.ecommerce.shopdaily.domain.repository.DummyJsonRepository
+import com.google.common.truth.Truth.assertThat
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class GetFavoritesUseCaseTest {
+    @Mock
+    private lateinit var mockRepository: DummyJsonRepository
+
+    private lateinit var useCase: GetFavoritesUseCase
+
+    @Before
+    fun setUp() {
+        useCase = GetFavoritesUseCase(mockRepository)
+    }
+
+    @Test
+    fun `invoke should emit Success resource when getFavorites is successful`() =
+        runBlocking {
+            val favorites = listOf(
+                ProductEntity(
+                    productId = 1,
+                    image = "https://example.com/images/product1.jpg",
+                    title = "Example Product 1",
+                    subtitle = "Lorem ipsum dolor sit amet",
+                    price = 100,
+                    discount = 0.2,
+                    rating = 4.5,
+                    description = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam euismod felis id sapien congue, a malesuada justo finibus.",
+                    images = listOf(
+                        "https://example.com/images/product1-1.jpg",
+                        "https://example.com/images/product1-2.jpg",
+                        "https://example.com/images/product1-3.jpg"
+                    ),
+                    category = "Electronics"
+                )
+            )
+            `when`(mockRepository.getFavorites()).thenReturn(favorites)
+            val expectedResource = Resource.Success(favorites.map { it.toProduct() })
+
+            var actualResource: Resource.Success<List<Product>>? = null
+
+            useCase().collect { result ->
+                if (result is Resource.Success) actualResource = result
+            }
+
+            assertThat(actualResource?.data).isEqualTo(expectedResource.data)
+        }
+
+    @Test
+    fun `invoke should emit Loading and Error resource when getFavorites throws an exception`() =
+        runBlocking {
+            val favorites = emptyList<ProductEntity>()
+            `when`(mockRepository.getFavorites()).thenReturn(favorites)
+            val expectedResource = Resource.Error<Nothing>("")
+
+            var actualResource: Resource.Error<List<Product>>? = null
+
+            useCase().collect { result ->
+                if (result is Resource.Error) actualResource = result
+            }
+
+            assertThat(actualResource?.message).isEqualTo(expectedResource.message)
+        }
+}
